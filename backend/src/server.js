0461561282f7842e9cf2cf58ea3b00512344d68f
sanjiep.magar <saasmagar554@gmail.com>
2025-06-05 22:39:02 +0300

    delpoyed bakend in render

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,14 +2,21 @@ import express from 'express';
 import 'dotenv/config';
 import { connectToDatabase } from './config/db.js';
 import ratelimiterMiddleware from './middleware/ratelimiter.js';
+import job from './config/cron.js';
 
 const app = express();
+
+if (process.env.NODE_ENV === "production") job.start(); // Start the cron job to send GET request every 14 minutes
 
 app.use(express.json()); // Middleware to parse JSON bodies
 app.use(ratelimiterMiddleware) // Apply rate limiting middleware
 import transactionsRoute from './routes/transactionsRoute.js';
 
 const PORT = process.env.PORT || 5001;
+
+app.get("/api/health", (req, res) => {
+    res.status(200).json({ message: "Server is healthy" });
+})
 
 
 app.use('/api/transactions', transactionsRoute)
--- mobile/app/(root)/index.jsx
+++ mobile/app/(root)/index.jsx
@@ -2,23 +2,32 @@ import { SignedIn, SignedOut, useUser } from '@clerk/clerk-expo'
 import { Link } from 'expo-router'
 import { Text, View } from 'react-native'
 import { SignOutButton } from '@/components/SignOutButton'
+import { useTransactions } from '../../hooks/useTransactions'
+import { useEffect } from 'react'
 
 export default function Page() {
   const { user } = useUser()
+  const {transactions, summary, loadData, isLoading, deleteTransaction} = useTransactions(user?.id)
+
+  useEffect(() => {
+    loadData()
+  }, [loadData, user?.id])
+
+  console.log('userId', user?.id);
+  console.log('transaction', transactions);
+  console.log('summary', summary);
+  
 
   return (
     <View>
       <SignedIn>
         <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
+        <Text>Total Income is {summary.income}</Text>
+        <Text>Total Balance is {summary.balance}</Text>
+        <Text>Total Expenses is {summary.expenses}</Text>
         <SignOutButton />
       </SignedIn>
       <SignedOut>
-        <Link href="/(auth)/sign-in">
-          <Text>Sign in</Text>
-        </Link>
-        <Link href="/(auth)/sign-up">
-          <Text>Sign up</Text>
-        </Link>
       </SignedOut>
     </View>
   )
